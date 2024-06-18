bebd32377e6d2b26bc6fbb5d7ac135aa92f176f3
fdaniall <fakhridanial29@gmail.com>
2024-06-18 21:44:09 +0700

    Update Title Dev

--- fdaniall/src/app/layout.js
+++ fdaniall/src/app/layout.js
@@ -2,8 +2,8 @@ import Header from "./Header";
 import "./globals.css";
 
 export const metadata = {
-  title: "Developer Page",
-  description: "Hello",
+  title: "Fakhri Danial",
+  description: "Personal Website of Fakhri Danial",
 };
 
 export default function RootLayout({ children }) {
